01f360e3645f3cac22481ac013d12777f2ac8735
Rafal Rekawica <rafal.rekawica@fujitsu.com>
2026-07-01 11:37:08 +0200

    Improved error message for E-12 error

--- dra-driver-image-configurator/internal/controller/pod_controller_test.go
+++ dra-driver-image-configurator/internal/controller/pod_controller_test.go
@@ -546,7 +546,7 @@ func TestReconcile(t *testing.T) {
 					BindingConditions: []string{BindingConditionUpdateImage},
 				}),
 			),
-			errMsg:      "no ImageConfig found in claims for pod",
+			errMsg:      "no valid ImageConfig found in claims matching the pod containers",
 			wantRequeue: false,
 		},
 		{
