97bd345de4f5684c3f4afa6e34d4c5256f62b3e0
Tsubasa Watanabe <w.tsubasa@fujitsu.com>
2026-07-02 19:43:35 +0900

    ImageConfig.ContainerName or Image is empty
    Signed-off-by: Tsubasa Watanabe <w.tsubasa@fujitsu.com>

--- dra-driver-image-configurator/internal/controller/pod_controller.go
+++ dra-driver-image-configurator/internal/controller/pod_controller.go
@@ -148,7 +148,7 @@ func collectImageConfigs(claims []*resourceapi.ResourceClaim) ([]*imagev1alpha1.
 			}
 			ic, ok := obj.(*imagev1alpha1.ImageConfig)
 			if !ok || ic.ContainerName == "" || ic.Image == "" {
-				continue
+				return nil, reconcile.TerminalError(fmt.Errorf("ContainerName or Image empty"))
 			}
 			imageConfigs = append(imageConfigs, ic)
 		}
